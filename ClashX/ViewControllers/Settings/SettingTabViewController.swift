ed3e3e7bb38e6fb69b6802d27744fd238d48f128
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2026-04-25 11:26:56 +0800

    Defer core tab creation until installation

--- ClashX/ViewControllers/Settings/SettingTabViewController.swift
+++ ClashX/ViewControllers/Settings/SettingTabViewController.swift
@@ -9,26 +9,7 @@
 import Cocoa
 
 class SettingTabViewController: NSTabViewController, NibLoadable {
-    private let coreViewController: CoreSettingViewController = {
-        let viewController = CoreSettingViewController()
-        _ = viewController.view
-        return viewController
-    }()
-
-    private lazy var coreTabViewItem: NSTabViewItem = {
-        let item = NSTabViewItem(viewController: coreViewController)
-        item.label = NSLocalizedString("Core", comment: "")
-        item.identifier = "Core"
-        if #available(macOS 11.0, *) {
-            item.image = NSImage(systemSymbolName: "cpu", accessibilityDescription: item.label)
-        }
-        return item
-    }()
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        ensureCoreTabInstalled()
-    }
+    private lazy var coreViewController = CoreSettingViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +26,12 @@ class SettingTabViewController: NSTabViewController, NibLoadable {
 
     private func ensureCoreTabInstalled() {
         guard !tabViewItems.contains(where: { $0.identifier as? String == "Core" }) else { return }
-        addTabViewItem(coreTabViewItem)
+        let item = NSTabViewItem(viewController: coreViewController)
+        item.label = NSLocalizedString("Core", comment: "")
+        item.identifier = "Core"
+        if #available(macOS 11.0, *) {
+            item.image = NSImage(systemSymbolName: "cpu", accessibilityDescription: item.label)
+        }
+        addTabViewItem(item)
     }
 }
